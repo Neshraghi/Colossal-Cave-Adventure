66764009ed299076ff2c87bb57f0ba34331dd8ae
Neda eshraghi <neda.eshraghi@gmail.com>
2018-11-15 12:01:25 -0800

    The re-implementation of Colossal-Cave-Adventure game!

---
+++ Socket.h
@@ -0,0 +1,48 @@
+
+
+#ifndef SOCKET_H
+#define SOCKET_H
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string>
+#include <arpa/inet.h>
+
+
+const int MAXHOSTNAME = 200;
+const int MAXCONNECTIONS = 5;
+const int MAXRECV = 600;
+
+class Socket
+{
+ public:
+  Socket();
+  virtual ~Socket();
+
+  bool create();
+  bool bind ( const int port );
+  bool listen() const;
+  bool accept ( Socket& ) const;
+
+
+  bool connect ( const std::string host, const int port );
+
+  bool send ( const std::string ) const;
+  int recv ( std::string& ) const;
+  void close();
+
+
+  bool is_valid() const { return m_sock != -1; }
+
+
+ private:
+
+  int m_sock;
+  sockaddr_in m_addr;
+
+};
+#endif
